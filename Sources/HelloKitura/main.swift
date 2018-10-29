2f293933d1be721db033ac028ac38c412dbfedce
[stringdoth] <[bharathb9595@gmail.com]>
2018-10-29 17:42:24 +0530

    make it better

---
+++ Sources/HelloKitura/main.swift
@@ -0,0 +1,24 @@
+import Kitura
+
+// Create a new router
+let router = Router()
+
+// Handle HTTP GET requests to /
+router.get("/wish") {
+    request, response, next in
+    response.send("Hello, World!")
+    next()
+}
+
+router.get("/SayHello") {
+    request, response, next in
+    response.send("Swift Says Hello")
+}
+
+
+
+// Add an HTTP server and connect it to the router
+Kitura.addHTTPServer(onPort: 8080, with: router)
+
+// Start the Kitura runloop (this call never returns)
+Kitura.run()
